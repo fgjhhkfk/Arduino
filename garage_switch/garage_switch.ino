62f3c1ac93071b29ea3636b65dbbebb312e604fd
fgjhhkfk <fgjhhkfk@googlemail.com>
2020-11-22 16:38:00 +0100

    Initial commit.

---
+++ garage_switch/garage_switch.ino
@@ -0,0 +1,227 @@
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <WIFIcredentials.h>
+
+const int output = 4;
+const int LED = 2;
+#define DHTPIN 25     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22     // DHT 22 (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+String readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+String readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(h);
+    return String(h);
+  }
+}
+
+// HTML web page
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>Garage</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="icon" type="image/png">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <style>
+    html {
+    font-family: Arial;
+    display: inline-block;
+    margin: 0px auto;
+    text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+        font-size: 1.5rem;
+        vertical-align:middle;
+        padding-bottom: 15px;
+    }
+    body { font-family: Arial; text-align: center; margin:0px auto; padding-top: 30px;}
+
+    .button {
+        display: block;
+        height: 200px;
+        width: 200px;
+        background-color: #008000;
+        border: none;
+        color: white;
+        padding: 0px 0px;
+        text-decoration: none;
+        font-size: 40px;
+        margin: 0px auto 35px;
+        cursor: pointer;
+        border-radius: 100px;
+        line-height: 190px;
+        box-shadow: 0px 4px 10px #002100;
+    }
+    .button:active{
+        box-shadow: 0px 0px 0px #002100;
+    }
+    </style>
+  </head>
+  <body>
+    <h1>Klick to open!</h1>
+    <p>
+      <button class="button" onmousedown="toggleCheckbox('on');" ontouchstart="toggleCheckbox('on');" onmouseup="toggleCheckbox('off');" ontouchend="toggleCheckbox('off');">Klick</button>
+      <script>
+      function toggleCheckbox(x) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/" + x, true);
+        xhr.send();
+      }
+      </script>
+    </p>
+    <p>
+      <i class="fas fa-thermometer-half" style="color:#059e8a;"></i>
+      <span class="dht-labels">Temperature</span>
+      <span id="temperature">%TEMPERATURE%</span>
+      <sup class="units">&deg;C</sup>
+    </p>
+    <p>
+      <i class="fas fa-tint" style="color:#00add6;"></i>
+      <span class="dht-labels">Humidity</span>
+      <span id="humidity">%HUMIDITY%</span>
+      <sup class="units">&percnt;</sup>
+    </p>
+
+</script>
+</body>
+<script>
+    setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("temperature").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/temperature", true);
+    xhttp.send();
+    }, 5000 ) ;
+
+    setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("humidity").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/humidity", true);
+    xhttp.send();
+    }, 5000 ) ;
+
+</script>
+</html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+AsyncWebServer server(80);
+
+void success()
+{
+  for(int i=0; i<3; i++)
+  {
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  } 
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  dht.begin();
+
+  pinMode(output, OUTPUT);
+  digitalWrite(output, LOW);
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(output, LOW);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println();
+  Serial.print("ESP IP Address: http://");
+  Serial.println(WiFi.localIP());
+  success();
+
+  if (!SPIFFS.begin()) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  pinMode(output, OUTPUT);
+  digitalWrite(output, LOW);
+
+  // Send web page to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/html", index_html);
+  });
+
+  // Receive an HTTP GET request
+  server.on("/on", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    digitalWrite(output, HIGH);
+    request->send(200, "text/plain", "ok");
+  });
+
+  // Receive an HTTP GET request
+  server.on("/off", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    digitalWrite(output, LOW);
+    request->send(200, "text/plain", "ok");
+  });
+
+  server.on("/icon", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/G.png", "image/png");
+  });
+
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+  });
+
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void loop() {
+
+}
